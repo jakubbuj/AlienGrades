ed6010af8b51a5b55bd592b5db3bc08855842c87
Hoydonckx, Jesse (Stud. DACS) <j.hoydonckx@student.maastrichtuniversity.nl>
2024-01-21 12:41:25 +0000

    Update 39 files
    - /src/main/java/com/example/javafxproject/BinaryDecisionTree.java
    - /src/main/java/com/example/javafxproject/DataPreparation.java
    - /src/main/java/com/example/javafxproject/ForestTests.java
    - /src/main/java/com/example/javafxproject/ForestTree.java
    - /src/main/java/com/example/javafxproject/HelloApplication.java
    - /src/main/java/com/example/javafxproject/HelloController.java
    - /src/main/java/com/example/javafxproject/Methods.java
    - /src/main/java/com/example/javafxproject/Node.java
    - /src/main/java/com/example/javafxproject/Phase3.java
    - /src/main/java/com/example/javafxproject/RandomForest.java
    - /src/main/java/com/example/javafxproject/TreeTests.java
    - /src/main/resources/com/example/javafxproject/CurrentGrades.csv
    - /src/main/resources/com/example/javafxproject/GraduateGrades.csv
    - /src/main/resources/com/example/javafxproject/StudentInfo.csv
    - /src/main/java/module-info.java
    - /target/classes/com/example/javafxproject/BinaryDecisionTree.class
    - /target/classes/com/example/javafxproject/CurrentGrades.csv
    - /target/classes/com/example/javafxproject/DataPreparation.class
    - /target/classes/com/example/javafxproject/ForestTests.class
    - /target/classes/com/example/javafxproject/ForestTree.class
    - /target/classes/com/example/javafxproject/GraduateGrades.csv
    - /target/classes/com/example/javafxproject/HelloApplication.class
    - /target/classes/com/example/javafxproject/HelloController$BoxPlot$Coordinates.class
    - /target/classes/com/example/javafxproject/HelloController$BoxPlot$Item.class
    - /target/classes/com/example/javafxproject/HelloController$BoxPlot.class
    - /target/classes/com/example/javafxproject/HelloController$BoxPlotScene.class
    - /target/classes/com/example/javafxproject/HelloController.class
    - /target/classes/com/example/javafxproject/Methods.class
    - /target/classes/com/example/javafxproject/Node.class
    - /target/classes/com/example/javafxproject/Phase3.class
    - /target/classes/com/example/javafxproject/RandomForest.class
    - /target/classes/com/example/javafxproject/StudentInfo.csv
    - /target/classes/com/example/javafxproject/TreeTests.class
    - /target/classes/module-info.class
    - /mvnw
    - /mvnw.cmd
    - /pom.xml
    - /QuestionsWeCanExpect.txt
    - /README.md

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxurwa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxproject to javafx.fxml;
-    exports com.example.javafxproject;
-}
